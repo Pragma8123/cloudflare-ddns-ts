02be6faf82049e5c6d041cdb7763e0e95fea2fde
Pragma8123 <max@machon.family>
2024-02-08 19:40:40 -0500

    refactor: code cleanup

--- src/external-ip/external-ip.service.ts
+++ src/external-ip/external-ip.service.ts
@@ -1,12 +1,28 @@
-import { Injectable } from '@nestjs/common';
-import { ExternalIpProvider } from './external-ip.provider';
+import { HttpService } from '@nestjs/axios';
+import { Injectable, Logger } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+class ExternalIpResponse {
+  public ip: string;
+}
 
 @Injectable()
 export class ExternalIpService {
-  constructor(private readonly externalIpProvider: ExternalIpProvider) {}
+  private readonly logger: Logger = new Logger(ExternalIpService.name);
+
+  constructor(private readonly httpService: HttpService) {}
 
   async getExternalIp(): Promise<string> {
-    const { ip } = await this.externalIpProvider.getExternalIp();
-    return ip;
+    const ipObservable = this.httpService
+      .get<ExternalIpResponse>('http://api.ipify.org?format=json')
+      .pipe(
+        map((response) => response.data.ip),
+        catchError((error) => {
+          this.logger.debug(error);
+          throw new Error('Failed to get external IP');
+        }),
+      );
+    return await firstValueFrom(ipObservable);
   }
 }
